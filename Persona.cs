0b7db43369c30bb1bb8bc16d577a3d0f57c72ebd
Itati02 <89601723+Itati02@users.noreply.github.com>
2021-10-21 21:04:15 -0500

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArreglos
+{
+    class Persona
+    {
+        private string nombre;
+        private string apellidos;
+        private DateTime? fechaNac;
+
+        public Persona(string nombre, string apellidos, DateTime? fechaNac)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.fechaNac = fechaNac;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string Apellidos
+        {
+            get
+            {
+                return apellidos;
+
+            }
+            set
+            {
+                apellidos = value;
+            }
+        }
+
+        public DateTime? FechaNac { get => fechaNac; set => fechaNac = value; }
+        public Persona()
+        {
+            nombre = "";
+            apellidos = "";
+            FechaNac = null;
+
+        }
+        public override string ToString()
+        {
+            return nombre+ ""+ apellidos+ ""+ fechaNac;
+        }
+    }
+
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppArreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] b = new string[5];
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = "Buen dia" + (i + 1);
+            }
+
+            for(int i=0; i< b.Length; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+
+            Persona[] A = new Persona[5];
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = new Persona();
+                Console.WriteLine("Nombre: ");
+                A[i].Nombre = Console.ReadLine();
+                Console.WriteLine("Apellidos:");
+                A[i].Apellidos = Console.ReadLine();
+                A[i].FechaNac = DateTime.Now;             
+            }
+            for(int i=0; i<A.Length; i++)
+            {
+                Console.WriteLine(A[i].ToString());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
